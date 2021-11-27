5a162188d21c4d36a498bef2eaee2680233df239
FREEZ2385 <schlee9703@naver.com>
2021-11-27 21:38:13 +0900

    [Update] add function of post

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,51 @@ var router = express.Router();
 router.get('/', function(req, res){
     res.send({test: 'hello World!'})
 })
+const connection = require('../sqlconnect');
+router.post('/insert-user', (req, res) => {
+    /**
+     * もらえるデータ
+     * firstName, lastName, email, password
+     */
+    res.set('Access-Control-Allow-Origin', '*');
+    connection.on("connect", err => {
+         if (err) {
+         console.error(err.message);
+         } else {
+         console.log("connected!");
+         InsertUserFunction(req.body.firstName, req.body.lastName,
+            req.body.email, req.body.password);
+         }
+     });
+    res.status(201)
+    })
 
+var Request = require('tedious').Request  
+var TYPES = require('tedious').TYPES;  
+
+function InsertUserFunction(firstName, lastName, email, password) {  
+    const request = new Request("INSERT dbo.UserInfo (FirstName, FamilyName, MailAddress, Password);", function(err) {  
+        if (err) {  
+        console.log(err);}  
+    });  
+    request.addParameter('FirstName', TYPES.NVarChar,firstName);  
+    request.addParameter('FamilyName', TYPES.NVarChar , lastName);  
+    request.addParameter('MailAddress', TYPES.NVarChar , email);  
+    request.addParameter('Password', TYPES.NVarChar ,password);  
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+            if (column.value === null) {  
+            console.log('NULL');  
+            } else {  
+            console.log("Product id of inserted item is " + column.value);  
+            }  
+        });  
+    });
+
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on("requestCompleted", function (rowCount, more) {
+        connection.close();
+    });
+    connection.execSql(request);  
+}  
 module.exports = router;
--- src/components/pages/register.tsx
+++ src/components/pages/register.tsx
@@ -1,24 +1,53 @@
 import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
 import './css/register.css'
-import { width } from '@mui/system';
 
 interface Props {
   text: string;
 }
 
+type T = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
 
 function Register({ text }: Props): JSX.Element {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  
+  const callBackendAPI = async (): Promise<T> => {
+    const requestOptions = {
+      crossDomain: true,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName, lastName, email, password })
+  };
+    const response = await fetch('http://localhost:3002/api/insert-user', requestOptions);
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
 
   return (
     <div className="register">
-        <TextField label="First Name" variant="outlined" />
-        <TextField label="Last Name" variant="outlined" />
-        <TextField label="Email" variant="outlined" />
-        <TextField label="Password" type="password" variant="outlined" />
-        <TextField label="Password confirm" type="password" variant="outlined" />
+        <TextField label="First Name" variant="outlined" onChange={(event) => setFirstName(event.target.value)}/>
+        <TextField label="Last Name" variant="outlined" onChange={(event) => setLastName(event.target.value)}/>
+        <TextField label="Email" variant="outlined" onChange={(event) => setEmail(event.target.value)}/>
+        <TextField label="Password" type="password" variant="outlined" onChange={(event) => setPassword(event.target.value)}/>
+        <TextField label="Password confirm" type="password" variant="outlined" onChange={(event) => setPasswordConfirmation(event.target.value)}/>
         <div>
-          <Button variant="contained">Register</Button>
+          <Button variant="contained" 
+          disabled={(password === '' || password !== passwordConfirmation)} 
+          onClick={() => callBackendAPI()}>Register</Button>
           <Button variant="contained">Cancel</Button>
         </div>
     </div>
