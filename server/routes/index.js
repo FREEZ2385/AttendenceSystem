188374e0ddc138ecc70699da7f524cda197b342d
FREEZ2385 <schlee9703@naver.com>
2021-11-28 11:52:29 +0900

    @]@

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,27 +9,36 @@ var connection = require('../sqlconnect');
 var Request = require('tedious').Request  
 var TYPES = require('tedious').TYPES;  
 
+/**  
+ *  DB接続
+ */
 connection.connect();
 connection.on("connect", err => {
     console.log("DB Connected!");
 });
+
 
 router.post('/insert-user', (req, res) => {
     /**
      * もらえるデータ
      * firstName, lastName, email, password
      */
-    connection.on("connect", err => {
-        InsertUserFunction(connection, req.body.firstName, req.body.lastName,
-            req.body.email, req.body.password);
-    });
+    console.log("Insert Connecting");
+    InsertUserFunction(req.body.firstName, req.body.lastName,
+        req.body.email, req.body.password);
     
     res.end("Insert Success")
 })
 
 
-
-function InsertUserFunction(connection, firstName, lastName, email, password) {  
+/**  
+ *  ユーザー登録
+ *  @param {string} firstName
+ *  @param {string} lastName
+ *  @param {string} email
+ *  @param {string} password
+ */
+function InsertUserFunction(firstName, lastName, email, password) {  
     const request = new Request("INSERT INTO UserInfo (FirstName,FamilyName,MailAddress,Password) VALUES (@FirstName,@FamilyName,@MailAddress,@Password);", function(err) {  
         if (err) {  
         console.log("error in request")
@@ -50,10 +59,10 @@ function InsertUserFunction(connection, firstName, lastName, email, password) {
     });
 
     // Close the connection after the final event emitted by the request, after the callback passes
-    request.on("requestCompleted", function (rowCount, more) {
+    request.on("requestCompleted", function () {
         console.log("insert connected!");
-        connection.close();
     });
     connection.execSql(request);  
 }  
+
 module.exports = router;
